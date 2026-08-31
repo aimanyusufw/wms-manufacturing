9e0c54b7ec00bf2f6c5e67f9ff5d6c73d8fe5340
aimanyusufw <aimanyusufdev@gmail.com>
2026-08-31 21:28:39 +0700

    feat: implement Customer resource with related models, policies, and migration

---
+++ app/Models/Customer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Customer extends Model
+{
+    use SoftDeletes, HasFactory, LogsActivity;
+
+    protected $table = 'customers';
+
+    protected $fillable = [
+        'code',
+        'name',
+        'contact_person',
+        'phone',
+        'country',
+        'email',
+        'address',
+        'tax_number',
+        'is_active'
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'is_active' => 'boolean',
+        ];
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logFillable()
+            ->logOnlyDirty()
+            ->dontSubmitEmptyLogs()
+            ->useLogName('product');
+    }
+}
---
+++ database/migrations/2026_08_31_141605_create_customers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string("code", 50)->unique();
+            $table->string("name", 200);
+            $table->string("contact_person", 150)->nullable();
+            $table->string("phone", 50)->nullable();
+            $table->string("email", 50)->nullable();
+            $table->string("country", 100)->nullable();
+            $table->text("address")->nullable();
+            $table->string("tax_number", 100)->nullable();
+            $table->boolean("is_active")->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Indexes
+            $table->index('code');
+            $table->index('name');
+            $table->index('is_active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
